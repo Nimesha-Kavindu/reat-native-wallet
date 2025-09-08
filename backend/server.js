b0afe9dbcd4e8ae92dcfe41700e3edbc3e66fab5
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-09-08 05:35:54 +0530

    manage get and delete requests

--- backend/server.js
+++ backend/server.js
@@ -29,22 +29,62 @@ async function initDB() {
     }
 }
 
-app.post('/api/transactions', async (req, res) => {
-    try {
-        const {title, amount, category, user_id} = req.body;
 
-        if(!title || !amount || !category || !user_id) {
-            return res.status(400).json({error: "Missing required fields"});
+app.get('/api/transactions/:userId', async (req, res) => {
+    try {
+
+        const { userId } = req.params;
+        if (!userId) {
+            return res.status(400).json({ error: "Missing userId parameter" });
         }
 
-        const transaction= await sql`INSERT INTO transactions (title, amount, category, user_id) VALUES (${title}, ${amount}, ${category}, ${user_id}) RETURNING *`;
+        const transactions = await sql`SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`;
+        res.status(200).json(transactions);
+
+    } catch (error) {
+        console.log("Error getting transaction :", error)
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+
+app.delete('/api/transactions/:id', async (req, res) => {
+    try {
+        
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ error: "Missing id parameter" });
+        }
+
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "Invalid id parameter" });
+        }
+
+        const deletedTransaction = await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+        res.status(200).json({ message: "Transaction deleted successfully", transaction: deletedTransaction });
+
+    } catch (error) {
+        console.log("Error deleting transaction :", error)
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+app.post('/api/transactions', async (req, res) => {
+    try {
+        const { title, amount, category, user_id } = req.body;
+
+        if (!title || !amount || !category || !user_id) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const transaction = await sql`INSERT INTO transactions (title, amount, category, user_id) VALUES (${title}, ${amount}, ${category}, ${user_id}) RETURNING *`;
 
         console.log(transaction);
-        res.status(201).json({message: "Transaction created successfully"});
+        res.status(201).json({ message: "Transaction created successfully" });
 
     } catch (error) {
         console.log("Error creating transaction :", error)
-        res.status(500).json({error: "Internal server error"});
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
